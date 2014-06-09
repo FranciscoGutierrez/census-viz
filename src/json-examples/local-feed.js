adb8da0755b4a39fa5775415c11481e9720ff2c5
franciscogutierrez <fsalvador23@gmail.com>
2014-06-09 12:14:48 -0500

    Updated SocialList Component

---
+++ src/json-examples/local-feed.js
@@ -0,0 +1,189 @@
+ [
+  {
+  "mayKnow": [
+              {
+              "picture": "http://placehold.it/42x42",
+              "company": "Katakana",
+              "guid": "e2441079-d532-4fe2-8ccc-c91aab80d45c",
+              "name": "Rosario Hines",
+              "location": "Garberville, Maryland"
+              },
+              {
+              "picture": "http://placehold.it/42x42",
+              "company": "Neurocell",
+              "guid": "44aa463e-ab6d-4005-9731-7a1a2c20a686",
+              "name": "Laura Pace",
+              "location": "Brogan, Massachusetts"
+              },
+              {
+              "picture": "http://placehold.it/42x42",
+              "company": "Farmage",
+              "guid": "01eab1a3-900e-4f89-a150-f382a77856e4",
+              "name": "Julie Fry",
+              "location": "Kennedyville, Arizona"
+              },
+              {
+              "picture": "http://placehold.it/42x42",
+              "company": "Geologix",
+              "guid": "e21c4e5f-e9c7-462a-9c19-3227d556b5fd",
+              "name": "Sheena Carson",
+              "location": "Fingerville, Michigan"
+              },
+              {
+              "picture": "http://placehold.it/42x42",
+              "company": "Satiance",
+              "guid": "5b181c9c-8f71-4837-9bfa-430115aad3d5",
+              "name": "Nita Haley",
+              "location": "Martinez, Oregon"
+              }
+              ],
+  "shouldKnow": [
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Zaggles",
+                 "guid": "3ba15185-ee17-4e03-a6fd-fbe8b865a961",
+                 "name": "Dunlap Finch",
+                 "location": "Klagetoh, New Mexico"
+                 },
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Futurity",
+                 "guid": "5199dd8e-6ef2-4df1-a2a2-6fba8d3dd3c1",
+                 "name": "Eve Boyer",
+                 "location": "Fairforest, West Virginia"
+                 },
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Elemantra",
+                 "guid": "d951e079-74f6-4388-80fc-8ccad563596f",
+                 "name": "Melody Christian",
+                 "location": "Montura, Georgia"
+                 },
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Medesign",
+                 "guid": "c8b2192c-479b-4cd8-ab3e-b41e9fac6daf",
+                 "name": "Haley Lara",
+                 "location": "Greenwich, South Carolina"
+                 },
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Premiant",
+                 "guid": "2c667582-5a11-4cf9-b733-6516a4941f6c",
+                 "name": "Madge Townsend",
+                 "location": "Bannock, Kansas"
+                 }
+                 ],
+  "opinionLeaders": [
+                     {
+                     "picture": "http://placehold.it/42x42",
+                     "company": "Spacewax",
+                     "guid": "ec3e450c-5eba-48f8-a470-1e93e14cb5f2",
+                     "name": "Denise Pope",
+                     "location": "Albany, Indiana"
+                     },
+                     {
+                     "picture": "http://placehold.it/42x42",
+                     "company": "Virxo",
+                     "guid": "177f02c7-58c8-4546-92ab-d0b42e590162",
+                     "name": "Katheryn Sargent",
+                     "location": "Westphalia, Hawaii"
+                     },
+                     {
+                     "picture": "http://placehold.it/42x42",
+                     "company": "Ovation",
+                     "guid": "912fed84-f2f5-4338-b67c-5f012ae77e80",
+                     "name": "Wallace Guerra",
+                     "location": "Wauhillau, Rhode Island"
+                     },
+                     {
+                     "picture": "http://placehold.it/42x42",
+                     "company": "Stelaecor",
+                     "guid": "8758ee24-263d-40c7-b164-4e3d2b5114a5",
+                     "name": "Ramirez Roberson",
+                     "location": "Denio, Alaska"
+                     },
+                     {
+                     "picture": "http://placehold.it/42x42",
+                     "company": "Medicroix",
+                     "guid": "39ee79f8-7ccd-4d8b-a458-bc55f3fd9bad",
+                     "name": "Fowler Norton",
+                     "location": "Brooktrails, Florida"
+                     }
+                     ],
+  "keyPeople": [
+                {
+                "picture": "http://placehold.it/42x42",
+                "company": "Kidgrease",
+                "guid": "7f68ce3b-263f-4f0d-9104-271faf8f7e25",
+                "name": "Terra Mcmillan",
+                "location": "Curtice, Utah"
+                },
+                {
+                "picture": "http://placehold.it/42x42",
+                "company": "Vertide",
+                "guid": "636796a0-9f41-4940-ba89-b6ca594c8256",
+                "name": "Willis Harrison",
+                "location": "Brambleton, Nebraska"
+                },
+                {
+                "picture": "http://placehold.it/42x42",
+                "company": "Oulu",
+                "guid": "ff74c537-0718-4216-808a-6cac143b4439",
+                "name": "David Quinn",
+                "location": "Summertown, Pennsylvania"
+                },
+                {
+                "picture": "http://placehold.it/42x42",
+                "company": "Bytrex",
+                "guid": "d1ceab65-2c2b-42ea-a2fe-042b3fea9c07",
+                "name": "Kathy Noel",
+                "location": "Eden, Kentucky"
+                },
+                {
+                "picture": "http://placehold.it/42x42",
+                "company": "Viasia",
+                "guid": "2e1dd0ae-f71d-4967-aa35-f647be129876",
+                "name": "Magdalena Patel",
+                "location": "Teasdale, New Hampshire"
+                }
+                ],
+  "peopleAway": [
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Recritube",
+                 "guid": "41d764bc-d86a-46d8-b337-0dc5e65d2426",
+                 "name": "Sharpe Wilkinson",
+                 "location": "Talpa, Delaware"
+                 },
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Circum",
+                 "guid": "d74776b7-0102-458b-8486-12ac2110e975",
+                 "name": "Farrell Bridges",
+                 "location": "Darrtown, Nevada"
+                 },
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Zillar",
+                 "guid": "7e77e6d7-7cca-4202-959d-42652faec0e6",
+                 "name": "Melanie Raymond",
+                 "location": "Summerfield, New Jersey"
+                 },
+                 {
+                 "picture": "http://placehold.it/42x42",
+                 "company": "Eventix",
+                 "guid": "9fe319e9-27f0-4bf0-ba86-62f93ac098eb",
+                 "name": "Candace Burt",
+                 "location": "Henrietta, Missouri"
+                 }, 
+                 {
+                 "picture": "http://placehold.it/42x42", 
+                 "company": "Genmom", 
+                 "guid": "887bb9f3-b5bc-4ce8-8cc3-41cb7ea84884", 
+                 "name": "Jodie Nunez", 
+                 "location": "Dowling, North Dakota"
+                 }
+                 ]
+  }
+  ]
--- src/socialList.js
+++ src/socialList.js
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 /* Social List Component.
  * Universidad de las Américas Puebla http://ict.udlap.mx
  * As part of REAUMOBILE project.
@@ -31,8 +30,8 @@
 document.registerElement('social-list', {
                          prototype: Object.create(HTMLElement.prototype)
                          });
-
-var jsonData = "http://www.json-generator.com/j/bPhrNYMCnC?indent=4";
+var prefix = "census";
+var jsonData = $("social-list").attr("data");
 var sLeftTab = "< Personas Interesantes";
 var scenterTab = "Personas que quizás conozcas";
 var srightTab = "Líderes de Opinión >";
@@ -45,25 +44,90 @@ $(".container").append("<div class=\"tab-bar\"></div>");
 $(".tab-bar").append("<div class=\"left-tab\">" + sLeftTab + "</div>");
 $(".tab-bar").append("<div class=\"center-tab\">" + scenterTab + "</div>");
 $(".tab-bar").append("<div class=\"right-tab\">" + srightTab + "</div>");
-$(".container").append("<div class=\"box-container\"></div>");
 
-$.getJSON(jsonData, function (data) {
-          var items = [];
-          $.each(data.mayKnow, function (key, value) {
-                 items.push("<div class=\"box\">");
-                 items.push("<div class=\"hidden-id\""+this.guid +"</div>");
-                 items.push("<div class=\"img-box\"><img src=" + this.picture + "/></div>");
-                 items.push("<div class=\"metadata-box\">");
-                 items.push("<div class=\"lead-meta\">"+ this.name + "</div>");
-                 // items.push("<div class=\"\">"+ this.company + "</div>");
-                 items.push("<div class=\"sub-meta\">"+ this.location + "</div></div>");
-                 items.push("<div class=\"action-box\">");
-                 items.push("<div class=\"first-action\"><button>Agregar</button></div>");
-                 items.push("<div class=\"second-action\"><button>Mensaje</button></div></div>");
-                 items.push("</div></div>");
-                 });
-          $("<div/>", {
-            "class": "box-container",
-            html: items.join("")
-            }).appendTo(".container");
-          });
+setupData("mayKnow");
+
+function setupData(datashow) {
+    $.getJSON(jsonData, function (data) {
+              var items = [];
+              var dataset;
+              switch (datashow) {
+              case "keyPeople":
+              dataset = data.keyPeople;
+              break;
+              case "opinionLeaders":
+              dataset = data.opinionLeaders;
+              break;
+              case "peopleAway":
+              dataset = data.peopleAway;
+              break;
+              case "mayKnow":
+              dataset = data.mayKnow;
+              break;
+              case "shouldKnow":
+              dataset = data.shouldKnow;
+              break;
+              }
+              $.each(dataset, function (key, value) {
+                     items.push("<div class=\"box\">");
+                     items.push("<div class=\"hidden-id\"" + this.guid + "</div>");
+                     items.push("<div class=\"img-box\"><img src=" + this.picture + "/></div>");
+                     items.push("<div class=\"metadata-box\">");
+                     items.push("<div class=\"lead-meta\">" + this.name + "</div>");
+                     // items.push("<div class=\"\">"+ this.company + "</div>");
+                     items.push("<div class=\"sub-meta\">" + this.location + "</div></div>");
+                     items.push("<div class=\"action-box\">");
+                     items.push("<button class=\"first-action\"");
+                     items.push("value=" + this.guid + ">Agregar</button>");
+                     items.push("<button class=\"second-action\"");
+                     items.push("value=" + this.guid + ">Mensaje</button></div>");
+                     items.push("</div></div>");
+                     });
+              $("<div/>", {
+                "class": "box-container",
+                html: items.join("")
+                }).appendTo(".container");
+              });
+}
+/* Event Handler*/
+$(".left-tab").click(function () {
+                     $(".box-container").fadeOut(100, function () {
+                                                 $(this).remove();
+                                                 if ($("div.center-tab").text() == "Personas que quizás conozcas") {
+                                                 $("div.right-tab").text("Personas que quizá conozcas >");
+                                                 $("div.center-tab").text("Personas Interesantes");
+                                                 $("div.left-tab").text("");
+                                                 setupData("keyPeople");
+                                                 } else if ($("div.center-tab").text() == "Líderes de Opinión") {
+                                                 $("div.right-tab").text(srightTab);
+                                                 $("div.center-tab").text(scenterTab);
+                                                 $("div.left-tab").text(sLeftTab);
+                                                 setupData("mayKnow");
+                                                 }
+                                                 });
+                     });
+
+$(".right-tab").click(function () {
+                      $(".box-container").fadeOut(100, function () {
+                                                  $(this).remove();
+                                                  if ($("div.center-tab").text() == "Personas que quizás conozcas") {
+                                                  $("div.right-tab").text("");
+                                                  $("div.center-tab").text("Líderes de Opinión");
+                                                  $("div.left-tab").text("< Personas que quizá conozcas");
+                                                  setupData("opinionLeaders");
+                                                  } else if ($("div.center-tab").text() == "Personas Interesantes") {
+                                                  $("div.right-tab").text(srightTab);
+                                                  $("div.center-tab").text(scenterTab);
+                                                  $("div.left-tab").text(sLeftTab);
+                                                  setupData("mayKnow");
+                                                  }
+                                                  });
+                      });
+
+$(".container").on("click", ".first-action", function () {
+                   eval($("social-list").attr("action1"));
+                   });
+
+$(".container").on("click", ".second-action", function () {
+                   eval($("social-list").attr("action2"));
+                   });
